0ec75cf4677ceb48c878fc045531686f5a7af3aa
wll <wll@itcast.com>
2017-02-17 21:16:09 +0800

    王丽丽--基本框架的搭建--初建

---
+++ app/src/main/java/com/bestteam/oschina/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.bestteam.oschina.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 王丽丽 on 2017/2/17.
+ */
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/bestteam/oschina/bean/ListEntity.java
@@ -0,0 +1,9 @@
+package com.bestteam.oschina.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface ListEntity<T extends Entity> extends Serializable {
+
+    public List<T> getList();
+}
