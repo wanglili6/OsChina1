f4dfb4cac28092839af30823a4e29ce725488d79
why <why@itcast.cn>
2017-02-21 12:41:53 +0800

    why

--- app/src/main/java/com/bestteam/oschina/adapter/NewsFragmentAdapter/NewsFragmentVPAdapter.java
+++ app/src/main/java/com/bestteam/oschina/adapter/NewsFragmentAdapter/NewsFragmentVPAdapter.java
@@ -1,4 +1,4 @@
-package com.bestteam.oschina.adapter;
+package com.bestteam.oschina.adapter.newsfragmentadapter;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
--- app/src/main/java/com/bestteam/oschina/base/Cantents.java
+++ app/src/main/java/com/bestteam/oschina/base/Cantents.java
@@ -1,5 +1,7 @@
 package com.bestteam.oschina.base;
 
+import java.security.PublicKey;
+
 /**
  * Created by 王丽丽 on 2017/2/19.
  * 存放常量的的接口
@@ -7,4 +9,6 @@ package com.bestteam.oschina.base;
 
 public interface Cantents {
     public String CLISSIFTY_URl = "https://www.oschina.net/action/api/softwarecatalog_list?tag=";
+
+    public String NEWS = "http://www.oschina.net/action/api/news_list?pageIndex=0&catalog=1&pageSize=20";
 }
