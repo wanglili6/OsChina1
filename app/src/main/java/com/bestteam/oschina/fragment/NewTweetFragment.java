138391ad33a93f39091b1bdbf684e8c1a26cc803
lihao <lihao@qq.com>
2017-02-21 17:16:17 +0800

    初步实现动弹 真实数据的下拉刷新和加载更多功能 存档以庆屌屌哒

--- app/src/main/java/com/bestteam/oschina/fragment/NewTweetFragment.java
+++ app/src/main/java/com/bestteam/oschina/fragment/NewTweetFragment.java
@@ -14,16 +14,21 @@ import com.bestteam.oschina.adapter.NewTweetAdapter;
 import com.bestteam.oschina.base.Cantents;
 import com.bestteam.oschina.bean.Tweet;
 import com.bestteam.oschina.bean.TweetsList;
+import com.bestteam.oschina.net.okhttp.interceptor.OKHttp3Helper;
 import com.bestteam.oschina.util.XmlUtils;
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+
+import static com.bestteam.oschina.activity.MyApplication.getContext;
 
 
 /**
@@ -35,22 +40,37 @@ public class NewTweetFragment extends BaseTweetFragment {
 
     private NewTweetAdapter adapter;
     private boolean isRefreshing;
+    private boolean isLoadingMore;
+    private int pageIndex = 0;
+    final private int PULL_REFRESH = 0;
+    final private int LOAD_MORE = 1;
 
-    /*private Handler handler = new Handler() {
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
-                case 1:
+                case PULL_REFRESH:
+                    if (isRefreshing) {
+                        isRefreshing = false;
+                        mRv.refreshComplete();
+                    }
                     adapter.refresh((TweetsList) msg.obj);
+                    break;
+                case LOAD_MORE:
+                    if (isLoadingMore) {
+                        isLoadingMore = false;
+                        mRv.loadMoreComplete();
+                    }
+                    adapter.loadMore((TweetsList) msg.obj);
                     break;
             }
         }
-    };*/
+    };
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        loadNetData();
+        loadNetData(PULL_REFRESH);
     }
 
     @Override
@@ -74,23 +94,67 @@ public class NewTweetFragment extends BaseTweetFragment {
             public void onRefresh() {
                 if (!isRefreshing) {
                     isRefreshing = true;
-                    loadNetData();
+                    loadNetData(PULL_REFRESH);
                     //mRv.refreshComplete();
                 }
             }
 
             @Override
             public void onLoadMore() {
-
+                if (!isLoadingMore) {
+                    isLoadingMore = true;
+                    loadNetData(LOAD_MORE);
+                }
             }
         });
     }
 
-    public void loadNetData() {
+    public void loadNetData(int flag) {
         String url = Cantents.BASE_URL_TWEET + Cantents.NEW_TWEET_URL;
 
-        OkHttpClient okHttpClient = new OkHttpClient();
+        OKHttp3Helper okHttp3Helper = OKHttp3Helper.create();
+        if (flag == PULL_REFRESH) {
+            okHttp3Helper.get(url, new OKHttp3Helper.HttpCallback() {
+                @Override
+                public void onSuccess(String data) {
+                    final TweetsList bean = XmlUtils.toBean(TweetsList.class, data.getBytes());
+                    pageIndex++;
+                    Message msg = new Message();
+                    msg.what = PULL_REFRESH;
+                    msg.obj = bean;
+                    handler.sendMessage(msg);
+                }
 
+                @Override
+                public void onFail(Exception e) {
+
+                }
+            });
+        } else if (flag == LOAD_MORE) {
+            Map<String, String> params = new HashMap<>();
+            params.put("uid", "0");
+            params.put("pageSize", "20");
+            params.put("pageIndex", String.valueOf(pageIndex));
+            okHttp3Helper.get(Cantents.BASE_URL_TWEET, null, params, new OKHttp3Helper.HttpCallback() {
+                @Override
+                public void onSuccess(String data) {
+                    pageIndex++;
+                    final TweetsList bean = XmlUtils.toBean(TweetsList.class, data.getBytes());
+
+                    Message msg = new Message();
+                    msg.what = LOAD_MORE;
+                    msg.obj = bean;
+                    handler.sendMessage(msg);
+                }
+
+                @Override
+                public void onFail(Exception e) {
+
+                }
+            });
+        }
+
+        /*OkHttpClient okHttpClient = new OkHttpClient();
         Request request = new Request.Builder()
                 .get()
                 .url(url)
@@ -107,14 +171,14 @@ public class NewTweetFragment extends BaseTweetFragment {
 
                 Log.i("testload", "成功" + response);
                 final TweetsList bean = XmlUtils.toBean(TweetsList.class, response.body().bytes());
-                /*Log.i("testload","成功"+bean.toString());
+                *//*Log.i("testload","成功"+bean.toString());
                 Log.i("testload","成功...."+bean.getTweetCount());
                 Log.i("testload","成功...."+bean.getPagesize());
-                Log.i("testload","成功...."+bean.getTweetslist().size());*/
-                /*Message msg = new Message();
+                Log.i("testload","成功...."+bean.getTweetslist().size());*//*
+                *//*Message msg = new Message();
                 msg.what = 1;
                 msg.obj = bean;
-                handler.sendMessage(msg);*/
+                handler.sendMessage(msg);*//*
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -127,6 +191,6 @@ public class NewTweetFragment extends BaseTweetFragment {
                     }
                 });
             }
-        });
+        });*/
     }
 }
