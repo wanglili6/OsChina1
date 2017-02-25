95b651c279202ac7aef91a7154e287873d662de6
lx <lx@itcast.com>
2017-02-25 17:19:03 +0800

    Gender 给你了

--- app/src/main/java/com/bestteam/oschina/base/Cantents.java
+++ app/src/main/java/com/bestteam/oschina/base/Cantents.java
@@ -37,6 +37,7 @@ public interface Cantents {
     String MY_UID = "uid";  //uid值
     String MY_USERNAME = "username";  //用户名
     String MY_PWD = "pwd";  //密码
+    String MY_GENDER = "gender";    //获得男女
 
 
 }
