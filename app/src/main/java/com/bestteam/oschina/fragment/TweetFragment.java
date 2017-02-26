68f4adced0a1e9da72bffcb48de809765b491d14
lihao <lihao@qq.com>
2017-02-26 12:19:46 +0800

    真的是最后一次提交tweet lihao

--- app/src/main/java/com/bestteam/oschina/fragment/TweetFragment.java
+++ app/src/main/java/com/bestteam/oschina/fragment/TweetFragment.java
@@ -1,5 +1,6 @@
 package com.bestteam.oschina.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
